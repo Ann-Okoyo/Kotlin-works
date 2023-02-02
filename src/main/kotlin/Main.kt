0388d178ea5d8adde77ee1a8b8b2a7e1fbcccf14
Muyale Okoyo <annmuyale@gmail.com>
2023-02-02 09:51:27 +0300

    kotlin coding

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+
+
+fun main(args: Array<String>) {
+    var fullName = "Ann Muyale"
+    var age  = 20
+    var phoneNumber  = "+25448403328"
+    var weightInKg : Short = 54
+    val kenyanCitizen : Boolean = true
+    val isNotKenyanCitizen : Boolean = false
+
+    println( fullName + age + phoneNumber + weightInKg + kenyanCitizen)
+
+}
+
