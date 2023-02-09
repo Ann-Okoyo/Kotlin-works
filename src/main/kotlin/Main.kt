e7e894be54b4f1fe3464d5da3c95ed3ae89737e8
Muyale Okoyo <annmuyale@gmail.com>
2023-02-09 07:55:46 +0300

    Functions kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
+fun main(){
 
 
-
-fun main(args: Array<String>) {
-    var fullName = "Ann Muyale"
-    var age  = 20
-    var phoneNumber  = "+25448403328"
-    var weightInKg : Short = 54
-    val kenyanCitizen : Boolean = true
-    val isNotKenyanCitizen : Boolean = false
-
-    println( fullName + age + phoneNumber + weightInKg + kenyanCitizen)
+//naming()
+    var result= div(46.4, 5.5)
+    println(result)
+    fact("Ann")
 
 }
 
+
+//fun naming() {
+//    var name ="Ada"
+//    println("Hello"+ "  " + name)
+//}
+
+fun div(num1: Double, num2: Double): Double {
+     var division =num1 % num2
+     return division
+}
+fun add(num1: Int, num2: Int,num3:Int,num4:Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+
+
+}
+fun fact(me:String){
+    println(me)
+}
